ee25e049b4c365fd4a8749cb17a7cb5e2c52a764
dakotahlake <23041745+dakotahlake@users.noreply.github.com>
2021-03-29 23:32:04 -0500

    tic tac toe working

--- src/index.js
+++ src/index.js
@@ -1,17 +1,115 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Square(props){
+    return(
+        <button
+            className="square"
+            onClick={props.onClick}
+        >
+            {props.value}
+        </button>
+    )
+}
+  
+class Board extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
+    }
+
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        if(squares[i] != null || calcWinner(squares)) return;
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    renderSquare(i) {
+        return (
+            <Square 
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
+    }
+
+    render() {
+        const winner = calcWinner(this.state.squares);
+        let status;
+        if (winner) {status = 'Winner: ' + winner;}
+        else {status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');}
+        
+
+        return (
+        <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+            </div>
+            <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+            </div>
+            <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+            </div>
+        </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+        <div className="game">
+            <div className="game-board">
+            <Board />
+            </div>
+            <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+            </div>
+        </div>
+        );
+    }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+<Game />,
+document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  
+function calcWinner(squares){
+    console.log(squares);
+    var diagonal1 = '000'; var diagonal2 = '000';
+    for(let i=0; i<3; i++){
+        if(squares[i*3] && squares[i*3] === squares[i*3 +1] && squares[i*3] === squares[i*3 +2]){
+            return squares[i*3];
+        }
+        if(squares[i] && squares[i] === squares[i +3] && squares[i] === squares[i +6]){
+            return squares[i];
+        }
+        diagonal1 = diagonal1.replace('0',squares[i*4])
+        diagonal2 = diagonal2.replace('0',squares[i*2 +2])
+    }
+    console.log(diagonal1)
+    if(squares[0] && diagonal1 === squares[0].repeat(3)) return squares[0];
+    if(squares[2] && diagonal2 === squares[2].repeat(3)) return squares[2];
+    return null;
+}
